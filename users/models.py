d88ed294b71f77b46ebc2adadd4527ecf21e1e6d
LsFbr <louisfabre@hotmail.fr>
2025-10-17 11:17:35 +0200

    feat(models): Implement custom UserManager for user creation and update User model to include birthday field

--- users/models.py
+++ users/models.py
@@ -1,29 +1,47 @@
 from django.contrib.auth.models import AbstractUser
-from django.core.exceptions import ValidationError
 from django.db import models
-from datetime import date
+from django.contrib.auth.base_user import BaseUserManager
 
 
-def validate_age(birthday: date):
-    if birthday is None:
-        return
-    today = date.today()
-    years = today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day))
-    if years < 15:
-        raise ValidationError("L'utilisateur doit avoir au moins 15 ans pour s'inscrire (RGPD).")
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The username must be set")
+        user = self.model(username=username, **extra_fields)
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self.create_user(username, password, **extra_fields)
 
 
 class User(AbstractUser):
     email = None
     first_name = None
     last_name = None
-    birthday = models.DateField(validators=[validate_age])
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["birthday"]
+
+    birthday = models.DateField(null=False, blank=False)
     can_be_contacted = models.BooleanField(default=False)
     can_data_be_shared = models.BooleanField(default=False)
     created_time = models.DateTimeField(auto_now_add=True)
 
-    EMAIL_FIELD = None
-    REQUIRED_FIELDS = []
+    objects = UserManager()
 
     def __str__(self):
         return self.username
