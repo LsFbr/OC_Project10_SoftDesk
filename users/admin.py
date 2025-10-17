2220c61c8d4c5bdda8ae1ae2e6f87444b3a8daad
LsFbr <louisfabre@hotmail.fr>
2025-10-17 11:21:38 +0200

    feat(admin): Update UserAdmin to include readonly fields and additional fields in add_fieldsets

--- users/admin.py
+++ users/admin.py
@@ -5,6 +5,8 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
+    readonly_fields = ("created_time", "last_login", "date_joined")
+    
     fieldsets = (
         (None, {"fields": ("username", "password")}),
         ("Informations personnelles", {"fields": ("birthday", "can_be_contacted", "can_data_be_shared")}),
@@ -12,7 +14,7 @@ class UserAdmin(BaseUserAdmin):
         ("Dates importantes", {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
-        (None, {"classes": ("wide",), "fields": ("username", "password1", "password2")}),
+        (None, {"classes": ("wide",), "fields": ("username", "password1", "password2", "birthday", "can_be_contacted", "can_data_be_shared")}),
     )
     list_display = ("username", "birthday", "can_be_contacted", "is_staff", "is_active")
     search_fields = ("username",)
