df08320239b7e413574ff94db0358d7c8ace4d9e
LsFbr <louisfabre@hotmail.fr>
2025-10-17 11:22:02 +0200

    feat(settings): Add Django REST framework and Simple JWT to INSTALLED_APPS; configure pagination and authentication settings.

--- softdesk/settings.py
+++ softdesk/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
     "users",
     "core",
 
@@ -125,3 +127,9 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': ()
+}
