3b652f5166e9d230cad352d5d38f70b976bb54de
LsFbr <louisfabre@hotmail.fr>
2025-10-17 15:38:24 +0200

    feat(views): Refactor user management views to include AdminUserViewSet for admin operations and enhance UserViewSet for read-only access, utilizing UserListSerializer and UserDetailSerializer for improved data handling.

--- users/views.py
+++ users/views.py
@@ -1,16 +1,36 @@
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from rest_framework.generics import CreateAPIView
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework import status
 
-from users.serializers import UserSerializer, RegisterSerializer
+from users.serializers import UserDetailSerializer, UserListSerializer, RegisterSerializer
 from users.models import User
 
-class UserViewSet(ModelViewSet):
-    serializer_class = UserSerializer
+class AdminUserViewSet(ModelViewSet):
+    serializer_class = UserListSerializer
+    detail_serializer_class = UserDetailSerializer
+    queryset = User.objects.all()
+
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return self.serializer_class
+        return self.detail_serializer_class 
+
+class UserViewSet(ReadOnlyModelViewSet):
+    serializer_class = UserListSerializer
+    detail_serializer_class = UserDetailSerializer
     queryset = User.objects.all()
 
     permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return self.serializer_class
+        return self.detail_serializer_class
     
 
 class RegisterView(CreateAPIView):
@@ -29,7 +49,7 @@ class RegisterView(CreateAPIView):
             "refresh": str(refresh),
         }
 
-        user_data = UserSerializer(user).data
+        user_data = UserDetailSerializer(user).data
         return Response(
             {"user": user_data, "tokens": tokens},
             status=status.HTTP_201_CREATED
