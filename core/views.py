54b60e499e5215d4ec7ae9ccf2eafc5272646cd6
LsFbr <louisfabre@hotmail.fr>
2025-10-24 12:18:15 +0200

    feat(views): Implement Project, Contributor, Issue, and Comment viewsets using ModelViewSet, enhancing API functionality with multiple serializers for detailed data representation.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from core.models import Contributor, Project, Issue, Comment
+from core.serializers import ProjectListSerializer, ProjectDetailSerializer, ContributorSerializer, IssueSerializer, CommentSerializer
 
-# Create your views here.
+class MultipleSerializerMixin:
+
+    detail_serializer_class = None
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve' and self.detail_serializer_class is not None:
+            return self.detail_serializer_class
+        return super().get_serializer_class()
+
+class ProjectViewSet(MultipleSerializerMixin, ModelViewSet):
+    serializer_class = ProjectListSerializer
+    detail_serializer_class = ProjectDetailSerializer
+    queryset = Project.objects.all()
+
+    def perform_create(self, serializer):
+        project = serializer.save(author=self.request.user)
+        Contributor.objects.get_or_create(
+            user=self.request.user,
+            project=project,
+            defaults={'role': 'AUTHOR'}
+        )
+
+
+
+class ContributorViewSet(ModelViewSet):
+    serializer_class = ContributorSerializer
+    queryset = Contributor.objects.all()
+
+
+class IssueViewSet(ModelViewSet):
+    serializer_class = IssueSerializer
+    queryset = Issue.objects.all()
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
