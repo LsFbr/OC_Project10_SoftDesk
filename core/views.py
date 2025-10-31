cde01365b05f6943efe3bc1a23bb849b474dbfa2
LsFbr <louisfabre@hotmail.fr>
2025-10-31 10:51:37 +0100

    feat(views): Introduce ContributorViewSet with project-specific querying and update ProjectViewSet to streamline contributor creation process.

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from core.models import Contributor, Project, Issue, Comment
-from core.serializers import ProjectListSerializer, ProjectDetailSerializer, ContributorSerializer, IssueSerializer, CommentSerializer
+from core.serializers import ProjectListSerializer, ProjectDetailSerializer, ContributorListSerializer, ContributorDetailSerializer, IssueSerializer, CommentSerializer
 
 class MultipleSerializerMixin:
 
@@ -14,21 +14,28 @@ class MultipleSerializerMixin:
 class ProjectViewSet(MultipleSerializerMixin, ModelViewSet):
     serializer_class = ProjectListSerializer
     detail_serializer_class = ProjectDetailSerializer
+    #queryset = Project.objects.select_related("author").prefetch_related("contributors__user", "issues")
     queryset = Project.objects.all()
 
     def perform_create(self, serializer):
         project = serializer.save(author=self.request.user)
-        Contributor.objects.get_or_create(
-            user=self.request.user,
-            project=project,
-            defaults={'role': 'AUTHOR'}
-        )
+        Contributor.objects.get_or_create(user=self.request.user, project=project)
 
+class ContributorViewSet(MultipleSerializerMixin, ModelViewSet):
+    serializer_class = ContributorListSerializer
+    detail_serializer_class = ContributorDetailSerializer
 
+    def get_queryset(self):
+        # /projects/{project_pk}/contributors/
+        project_id = self.kwargs.get('project_pk')
+        queryset = Contributor.objects.select_related("user", "project")
+        return queryset.filter(project_id=project_id) if project_id else queryset
 
-class ContributorViewSet(ModelViewSet):
-    serializer_class = ContributorSerializer
-    queryset = Contributor.objects.all()
+    def perform_create(self, serializer):
+        project_id = self.kwargs.get('project_pk')
+        if not project_id:
+            raise ValidationError("Project context is required.")
+        serializer.save(project_id=project_id) 
 
 
 class IssueViewSet(ModelViewSet):
