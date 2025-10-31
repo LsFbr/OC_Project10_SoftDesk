7f2b9b76ae7002205d539418b3b789cd5557ad64
LsFbr <louisfabre@hotmail.fr>
2025-10-31 10:52:41 +0100

    feat(admin): Register Project and Contributor models in the admin interface with customized list displays for improved management and visibility.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import Project, Contributor, Issue, Comment
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description", "type", "author", "created_time")
+
+@admin.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "project", "created_time")
+
