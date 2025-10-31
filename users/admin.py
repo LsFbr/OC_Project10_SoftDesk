00d1d1ad595bb7c373d328717fa4953e49a2e0c5
LsFbr <louisfabre@hotmail.fr>
2025-10-31 10:50:52 +0100

    feat(admin): Update UserAdmin list_display to include is_superuser for enhanced user role visibility in the admin interface.

--- users/admin.py
+++ users/admin.py
@@ -5,14 +5,13 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ("id", "username", "birthday", "can_be_contacted", "is_staff", "is_active", "created_time")
+    list_display = ("id", "username", "birthday", "can_be_contacted", "is_staff", "is_active", "is_superuser", "created_time")
 
     readonly_fields = ("created_time", "last_login", "date_joined")
     
     fieldsets = (
         (None, {"fields": ("username", "password")}),
         ("Informations personnelles", {"fields": ("birthday", "can_be_contacted", "can_data_be_shared")}),
-        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
         ("Dates importantes", {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
